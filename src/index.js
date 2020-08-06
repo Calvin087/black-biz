580828675308bf16467ca6bb6da6538c443437a7
Calvin-T <calvin+1@sliame.com>
2020-08-06 14:00:24 +0200

    add react-router app.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
+import AppRouter from './routers/AppRouter'
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
+      <AppRouter />  
+    </BrowserRouter>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
