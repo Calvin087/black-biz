aa6de73254ae2cf4d34db67a9adf98be3c6ccf0a
Calvin-T <calvin+1@sliame.com>
2020-08-07 17:55:47 +0200

    updates

--- src/components/FullWidthIntroLeft.js
+++ src/components/FullWidthIntroLeft.js
@@ -1,21 +1,28 @@
 import React from "react";
-import ButtonUI from '../components/ButtonUI'
+import ButtonUI from "../components/ButtonUI";
 
 const FullWidthIntroLeft = (props) => {
   return (
-    <div>
+    <div
+      className="m-auto sm:flex mb-10 sm:mb-16"
+      style={{ flexDirection: props.order }}
+    >
       <div className="z-0 -mb-4">
         <img src={props.image} />
       </div>
 
+      {/* Start Of Background */}
       <div
-        className="p-6 mx-4 mb-6 z-10 relative"
+        className="p-6 mx-4 sm:mx-0 sm:-mt-4 z-10 relative sm:p-16 sm:w-2/5"
         style={{ backgroundColor: props.bgColour }}
       >
-        <h2 className="text-2xl font-light pb-4" style={{ color: "#d3ccb5" }}>
+        <h2
+          className="text-2xl sm:text-3xl font-thin pb-4"
+          style={{ color: "#d3ccb5" }}
+        >
           {props.title}
         </h2>
-        <p className="mb-4 text-white font-thin">
+        <p className="mb-4 text-white font-thin sm:text-xl sm:mb-10">
           {props.content}
         </p>
         <ButtonUI
@@ -24,6 +31,7 @@ const FullWidthIntroLeft = (props) => {
           color={"#f1f1f1"}
         />
       </div>
+      {/* End Of Background */}
     </div>
   );
 };
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -12,13 +12,15 @@ const HomePage = () => {
       <div id="hero" className="sm:flex sm:px-16 sm:pt-6 container m-auto">
         {/* Start Of Hero Left */}
         <div id="hero-left" className="p-8 mt-4 sm:p-0 sm:w-2/4 sm:pr-24">
-          
-        <LogoBarOnly size={"50%"} />
+          <LogoBarOnly size={"50%"} />
 
-          <h2 className="text-4xl sm:text-5xl mt-8 pb-2 sm:mt-24" style={{color: "#16213a"}}>
+          <h2
+            className="text-4xl sm:text-5xl mt-8 pb-2 sm:mt-24"
+            style={{ color: "#16213a" }}
+          >
             Supporting The Black Economy.
           </h2>
-          
+
           <p className="text-l text-gray-800 font-thin mb-8 sm:text-2xl">
             We aim to curate and share the most powerful content to aid in the
             success of the generations that will follow us.
@@ -44,7 +46,7 @@ const HomePage = () => {
 
       {/* Start Of Four Block */}
 
-      <div className="my-6 sm:flex sm:-mt-48 sm:mb-48 sm:container sm:m-auto sm:pl-10">
+      <div className="my-6 sm:flex sm:-mt-48 sm:mb-40 sm:container sm:m-auto sm:pl-10">
         <HomeFourBlock
           bgColour={"#16213a"}
           title={"Community Support"}
@@ -68,43 +70,46 @@ const HomePage = () => {
       </div>
 
       {/* End Of Four Block */}
-
-      <FullWidthIntroLeft
-        bgColour={"#16213a"}
-        title={"Local Black Owned Business List."}
-        content={
-          "Unlike other directories, our black owned business list is 100% free. Each listing is manually audited to ensure legitimate businesses listings."
-        }
-        image={"../images/full-width-intro-biz.jpg"}
-        buttonContent={"Shop Local"}
-      />
-      <FullWidthIntroLeft
-        bgColour={"#1e3142"}
-        title={"Books on Essential Financial Literacy"}
-        content={
-          "Ensuring that our children are financially literate is the key to generational success and continued community growth."
-        }
-        image={"../images/full-width-intro-books.jpg"}
-        buttonContent={"Start Investing"}
-      />
-      <FullWidthIntroLeft
-        bgColour={"#27444c"}
-        title={"Entreprenuer & CEO Interviews"}
-        content={
-          "A collection of interviews and inspirational videos of black business owners. Learn how the beat the odds and how you can do it too."
-        }
-        image={"../images/full-width-intro-ceo.jpg"}
-        buttonContent={"Get Inspired"}
-      />
-      <FullWidthIntroLeft
-        bgColour={"#335446"}
-        title={"Online Courses & Free Education"}
-        content={
-          "Software development is a lucrative career that is lacking diversity. Change the industry with our list of free courses and learning resources."
-        }
-        image={"../images/full-width-intro-edu.jpg"}
-        buttonContent={"Earn More"}
-      />
+      <div className="container sm:px-16 sm:m-auto">
+        <FullWidthIntroLeft
+          bgColour={"#16213a"}
+          title={"Local Black Owned Business List."}
+          content={
+            "Unlike other directories, our black owned business list is 100% free. Each listing is manually audited to ensure legitimate businesses listings."
+          }
+          image={"../images/full-width-intro-biz.jpg"}
+          buttonContent={"Shop Local"}
+          order={"row-reverse"}
+        />
+        <FullWidthIntroLeft
+          bgColour={"#1e3142"}
+          title={"Books on Essential Financial Literacy"}
+          content={
+            "Ensuring that our children are financially literate is the key to generational success and continued community growth."
+          }
+          image={"../images/full-width-intro-books.jpg"}
+          buttonContent={"Start Investing"}
+        />
+        <FullWidthIntroLeft
+          bgColour={"#27444c"}
+          title={"Entreprenuer & CEO Interviews"}
+          content={
+            "A collection of interviews and inspirational videos of black business owners. Learn how the beat the odds and how you can do it too."
+          }
+          image={"../images/full-width-intro-ceo.jpg"}
+          buttonContent={"Get Inspired"}
+          order={"row-reverse"}
+        />
+        <FullWidthIntroLeft
+          bgColour={"#335446"}
+          title={"Online Courses & Free Education"}
+          content={
+            "Software development is a lucrative career that is lacking diversity. Change the industry with our list of free courses and learning resources."
+          }
+          image={"../images/full-width-intro-edu.jpg"}
+          buttonContent={"Earn More"}
+        />
+      </div>
 
       <div>
         <Footer />
